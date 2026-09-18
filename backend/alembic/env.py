fdb2caa5aaa0262ac7c6c28d40e615dd90a4b4db
anirudh <anirudhs.ansh@gmail.com>
2026-09-18 15:57:21 +0530

    style: clean backend imports

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,13 +1,10 @@
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from app.core.config import settings
 from app.core.database import Base
-
-import app.models
-
 
 config = context.config
 
